bb1fda3a2a255c9e3939d1b556f6b0566e944de0
Sofia Celi <sceli@thoughtworks.com>
2017-02-14 16:52:19 -0500

    implements generator following Fan's code

--- generator.go
+++ generator.go
@@ -1,41 +1,56 @@
 package ed448
 
-//void
-//find_generator()
-//{
-//    decaf_448_point_t p;
-//    unsigned char seed[decaf_448_ser_bytes+8];
-//    const char *magic = "g2";
-//    unsigned char encoded_point[decaf_448_ser_bytes],
-//                encoded_base[decaf_448_ser_bytes],
-//                hashed_base[decaf_448_ser_bytes];
-//
-//    decaf_448_point_encode(encoded_base, decaf_448_point_base);
-//    keccak_sponge_t sponge;
-//    shake256_init(sponge);
-//    shake256_update(sponge, (const unsigned char *)encoded_base, sizeof(encoded_base));
-//    shake256_final(sponge, hashed_base, sizeof(hashed_base));
-//    decaf_bool_t valid = decaf_false;
-//    int n=0;
-//    do {
-//        keccak_sponge_t magic_sponge;
-//        shake256_init(magic_sponge);
-//        int i;
-//        for (i=0; i<n; i++){
-//            shake256_update(magic_sponge, (const unsigned char *)magic, strlen(magic));
-//        }
-//        shake256_final(magic_sponge, seed, sizeof(seed));
-//        shake256_destroy(magic_sponge);
-//        /* random_bytes(seed,sizeof(seed)); */
-//        shake256_init(sponge);
-//        shake256_update(sponge, (const unsigned char *)magic, strlen(magic));
-//        shake256_update(sponge, (const unsigned char *)hashed_base, sizeof(hashed_base));
-//        shake256_update(sponge, (const unsigned char *)seed, sizeof(seed));
-//        shake256_final(sponge, encoded_point, sizeof(encoded_point));
-//        valid = decaf_448_point_decode(p, encoded_point, decaf_false);
-//        n++;
-//        printf("trial n:%d\n",n);
-//    } while (!valid);
-//    shake256_destroy(sponge);
-//    print_point(p,"g2 found");
-//}
+import (
+	"fmt"
+
+	"golang.org/x/crypto/sha3"
+)
+
+// this is not 512 as MH said. is it better to use elligator?
+// std::string seed = [a fixed 512-bit constant which you chose at random];
+// Point::from_hash(SHAKE<256>::Hash(std::string(Point::base()) + seed, Point::HASH_BYTES*2))
+func findGenerator() *twExtendedPoint {
+
+	var encodedBase [56]byte
+	var encodedPoint [56]byte
+	var seed [64]byte       //sure?
+	var hashedBase [56]byte //sure?
+
+	BasePoint.decafEncode(encodedBase[:])
+
+	h1 := sha3.NewShake256()
+	h1.Write(encodedBase[:])
+	h1.Read(hashedBase[:])
+
+	n := 0
+
+	p := &twExtendedPoint{
+		&bigNumber{},
+		&bigNumber{},
+		&bigNumber{},
+		&bigNumber{},
+	}
+
+	valid := word(0x00)
+
+	for valid == word(0x00) {
+		h2 := sha3.NewShake256()
+		for i := 0; i < n; i++ {
+			h2.Write([]byte("g2")) //why?
+		}
+		h2.Read(seed[:])
+
+		h3 := sha3.NewShake256()
+		h3.Write([]byte("g2"))
+		h3.Write(hashedBase[:]) //sure?
+		h3.Write(seed[:])       //sure?
+		h3.Read(encodedPoint[:])
+
+		valid = decafDecode(p, encodedPoint, false) // not allowing identity?
+		n++
+		fmt.Printf("trying %d \n", n)
+	}
+
+	fmt.Printf("found %d", p)
+	return p
+}
