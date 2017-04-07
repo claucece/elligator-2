43a134cfa25f19afe4d621ea27f2bc78bf93010b
Sofia Celi <sceli@thoughtworks.com>
2017-04-07 11:03:33 -0500

    adds c sketch

--- decaf_scalar.go
+++ decaf_scalar.go
@@ -1,3 +1,16 @@
 package ed448
 
 type decafScalar [scalarWords]word
+
+func serializeBytes(dst []byte, n *bigNumber) {
+	src := n.copy()
+	src.strongReduce()
+
+	for i := 0; i < 8; i++ {
+		l := dword(src[2*i]) + dword(src[2*i+1])<<28
+		for j := 0; j < 7; j++ {
+			dst[7*i+j] = byte(l)
+			l >>= 8
+		}
+	}
+}
--- keys.go
+++ keys.go
@@ -63,15 +63,67 @@ func generateKeys2(rand io.Reader) (publicKey, privateKey, error) {
 	return priv, pub, nil
 }
 
-func serializeBytes(dst []byte, n *bigNumber) {
-	src := n.copy()
-	src.strongReduce()
+// On C
 
-	for i := 0; i < 8; i++ {
-		l := dword(src[2*i]) + dword(src[2*i+1])<<28
-		for j := 0; j < 7; j++ {
-			dst[7*i+j] = byte(l)
-			l >>= 8
-		}
-	}
-}
+// XXX: change the types
+//static void inline
+//random_bytes_long_term(void * const buf, const size_t size) {
+//#ifndef FAST_RANDOM
+//  gcry_randomize(buf, size, GCRY_VERY_STRONG_RANDOM);
+//#else
+//  gcry_randomize(buf, size, GCRY_STRONG_RANDOM);
+//#endif
+//}
+//
+//static void inline
+//random_bytes_strong(void * const buf, const size_t size) {
+//  gcry_randomize(buf, size, GCRY_STRONG_RANDOM);
+//}
+
+// typedef unsigned char symmetric_key_t[32];
+//
+//int
+//derive_private_key (
+//    struct goldilocks_private_key_t *privkey,
+//    const unsigned char proto[GOLDI_SYMKEY_BYTES]
+//) {
+//
+//    const char *magic = "derive_private_key";
+//    uint8_t encoded_scalar[DECAF_448_SCALAR_OVERKILL_BYTES];
+//    decaf_448_point_t pub;
+//
+//    symmetric_key_t proto;
+//    random_bytes_strong(proto,sizeof(proto));
+//
+//    // take this from decaf brach or similar
+//    keccak_sponge_t sponge;
+//    shake256_init(sponge);
+//    shake256_update(sponge, proto, sizeof(symmetric_key_t));
+//
+//    // not needed part
+//    shake256_update(sponge, (const unsigned char *)magic, strlen(magic));
+//
+//    shake256_final(sponge, encoded_scalar, sizeof(encoded_scalar));
+//    shake256_destroy(sponge);
+//
+//    // is this how is done in C?
+//    encoded_scalar[55] &= 0;
+//    encoded_scalar[0] |= 1;
+//    encoded_scalar[0] &= 0;
+//
+//    barrett_deserialize_and_reduce(sk, encoded_scalar, sizeof(encoded_scalar), &curve_prime_order);
+//    barrett_serialize(privkey->opaque, sk, GOLDI_FIELD_BYTES);
+//
+//    tw_extensible_a_t exta;
+//    scalarmul_fixed_base(exta, sk, 446, &goldilocks_global.fixed_base);
+//
+//    // do a check if not, return nil
+//    mask_t ok;
+//    ok = validate_tw_extensible(exta)
+//
+//    untwist_and_double_and_serialize(pk, exta);
+//
+//    field_serialize(&privkey->opaque[446], pk);
+//
+//    return GOLDI_EOK;
+//}
